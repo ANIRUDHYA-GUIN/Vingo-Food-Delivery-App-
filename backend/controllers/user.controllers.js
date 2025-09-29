5f9158f40ca6a48f9bb540bb4918a97a277a0dc4
ANIRUDHYA-GUIN <anirudhyaguin2004@gmail.com>
2025-09-29 11:37:10 +0530

    Food Deivery app

--- backend/controllers/user.controllers.js
+++ backend/controllers/user.controllers.js
@@ -35,3 +35,5 @@ export const updateUserLocation=async (req,res) => {
     }
 }
 
+
+
--- frontend/firebase.js
+++ frontend/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "vingo-food-delivery.firebaseapp.com",
-  projectId: "vingo-food-delivery",
-  storageBucket: "vingo-food-delivery.firebasestorage.app",
-  messagingSenderId: "693314883513",
-  appId: "1:693314883513:web:4840c943ecf3fb61a33068"
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: "vigno-food-delivery.firebaseapp.com",
+  projectId: "vigno-food-delivery",
+  storageBucket: "vigno-food-delivery.firebasestorage.app",
+  messagingSenderId: "188327626530",
+  appId: "1:188327626530:web:6bffebeb65a1e74b71c77b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth=getAuth(app)
-export {app,auth}
+const provider=new GoogleAuthProvider()
+export {provider,auth}
